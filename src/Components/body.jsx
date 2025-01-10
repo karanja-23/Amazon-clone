ba44d12aed7bfbdeabc78f051b8bf022d1054072
Hosea <karanja.mungai23@gmail.com>
2025-01-10 13:43:46 +0300

    added barner

--- src/Components/body.jsx
+++ src/Components/body.jsx
@@ -4,6 +4,8 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import image from "../Assets/landing.jpg"
 import incentiveImage from "../Assets/incentives.jpg"
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 function Body() {
   return (
     <div id="body">
@@ -78,6 +80,31 @@ function Body() {
             <span>New Seller Incentives</span>
           </div>
           <span>Get started with $50,000 in incentives</span>
+          <p>
+          Ready to sell with Amazon? As a new seller, you can take advantage of a series of incentives.
+          </p>
+          <div id="offers">
+              <div className="offer">
+              <FontAwesomeIcon className="check" icon={faCheck} />
+              10% back on your first $50,000 in branded sales, then 5% back through your first year until you reach $1,000,000
+              </div>
+              <div className="offer">
+              <FontAwesomeIcon className="check" icon={faCheck} />
+              $100 off shipments into the Amazon fulfillment network using the Amazon Partnered Carrier program
+              </div>
+              <div className="offer">
+              <FontAwesomeIcon className="check" icon={faCheck} />
+              Free storage and customer returns with auto-enrollment in the FBA New Selection program
+              </div>
+              <div className="offer">
+              <FontAwesomeIcon className="check" icon={faCheck} />
+              $50 credit to create Sponsored Products or Sponsored Brands ads
+              </div>      
+          </div>
+          <div style={{display: "flex", alignItems: "center"}}>
+            <h4>See all incentives</h4>
+            <FontAwesomeIcon style={{fontSize: "0.9em", marginLeft: "9px"}} icon={faArrowRight} />
+          </div>
         </div>
       </div>
     </div>
